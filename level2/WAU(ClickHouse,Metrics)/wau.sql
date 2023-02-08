e75895e513cf07c357250e7f53b32bc79b1529fc
Maksim <123@gmail.com>
2023-02-08 23:03:54 +0300

    wau query

---
+++ level2/WAU(ClickHouse,Metrics)/wau.sql
@@ -0,0 +1,9 @@
+SELECT
+  DISTINCT toDate(timestamp) AS day,
+  COUNT(DISTINCT user_id) OVER (
+    ORDER BY
+      day RANGE BETWEEN 6 PRECEDING
+      AND CURRENT ROW
+  ) AS wau
+FROM
+  default.churn_submits
